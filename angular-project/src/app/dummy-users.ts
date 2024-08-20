0bd70e33d7212723b4c452f3307eb7cf6095d13a
Lavínia <laviniaof@hotmail.com>
2024-08-20 08:25:44 -0300

    add user component

---
+++ angular-project/src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Sheldon Cooper',
+    avatar: 'user-1.jpeg',
+  },
+  {
+    id: 'u2',
+    name: 'Leonard Hofstader',
+    avatar: 'user-2.jpeg',
+  },
+  {
+    id: 'u3',
+    name: 'Penny',
+    avatar: 'user-3.jpeg',
+  },
+  {
+    id: 'u4',
+    name: 'Howard Wolowitz',
+    avatar: 'user-4.jpeg',
+  },
+  {
+    id: 'u5',
+    name: 'Rajesh Kotrapaly',
+    avatar: 'user-5.jpeg',
+  },
+  {
+    id: 'u6',
+    name: 'Amy Farrah Fawler',
+    avatar: 'user-6.jpeg',
+  },
+];
