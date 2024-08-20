ea98b7757370a8b3f5dcd3881af41556173864d0
Lavínia <laviniaof@hotmail.com>
2024-08-20 08:32:41 -0300

    rename users img

--- angular-project/src/app/dummy-users.ts
+++ angular-project/src/app/dummy-users.ts
@@ -2,31 +2,31 @@ export const DUMMY_USERS = [
   {
     id: 'u1',
     name: 'Sheldon Cooper',
-    avatar: 'user-1.jpeg',
+    avatar: 'user-1.jpg',
   },
   {
     id: 'u2',
     name: 'Leonard Hofstader',
-    avatar: 'user-2.jpeg',
+    avatar: 'user-2.jpg',
   },
   {
     id: 'u3',
     name: 'Penny',
-    avatar: 'user-3.jpeg',
+    avatar: 'user-3.jpg',
   },
   {
     id: 'u4',
     name: 'Howard Wolowitz',
-    avatar: 'user-4.jpeg',
+    avatar: 'user-4.jpg',
   },
   {
     id: 'u5',
     name: 'Rajesh Kotrapaly',
-    avatar: 'user-5.jpeg',
+    avatar: 'user-5.jpg',
   },
   {
     id: 'u6',
     name: 'Amy Farrah Fawler',
-    avatar: 'user-6.jpeg',
+    avatar: 'user-6.jpg',
   },
 ];
