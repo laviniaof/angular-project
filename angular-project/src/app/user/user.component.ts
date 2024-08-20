94e4ca9d65fffe0edc860d0abbf73c23ff7311ad
Lavínia <laviniaof@hotmail.com>
2024-08-20 09:07:17 -0300

    imple. event listener

--- angular-project/src/app/user/user.component.ts
+++ angular-project/src/app/user/user.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { DYNAMIC_TYPE } from '@angular/compiler';
-
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -12,4 +12,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
