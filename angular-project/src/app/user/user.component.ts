90f294eb2a62a8490d912d29b005959f31221c94
Lavínia <laviniaof@hotmail.com>
2024-08-20 14:21:43 -0300

    config input decorator

--- angular-project/src/app/user/user.component.ts
+++ angular-project/src/app/user/user.component.ts
@@ -1,6 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,17 +8,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  //** Signal - stores a value and Angular will be able to see all the places that are using the value. It will manage subscriptions to the signal to get notified about value changes */
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+  onSelectUser() {}
 }
