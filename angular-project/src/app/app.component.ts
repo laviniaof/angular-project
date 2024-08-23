f6f8cce9bf0d2c8d25f8743f33afde3e609cc82e
Lavínia <laviniaof@hotmail.com>
2024-08-23 09:25:46 -0300

    applying clean code

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -14,10 +14,11 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  //Pode-se específicar qualquer id, não necessariamente o 'u1'.
   selectedUserId = 'u1';
 
   get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId)!;
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
   onSelectUser(id: string) {
