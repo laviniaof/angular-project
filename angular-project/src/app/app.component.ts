5cca1f669da20ce138a2ea6b3cbf48b8048cefc2
Lavínia <laviniaof@hotmail.com>
2024-08-26 08:16:05 -0300

    implem. new task component

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -5,10 +5,11 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './tasks/task/task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
