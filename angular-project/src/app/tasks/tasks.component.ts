f22e661c05d299b8820bc03c189dfc435410640b
Lavínia <laviniaof@hotmail.com>
2024-08-27 08:14:38 -0300

    implem. task component

--- angular-project/src/app/tasks/tasks.component.ts
+++ angular-project/src/app/tasks/tasks.component.ts
@@ -9,5 +9,33 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) userId!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced of Angular framework',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Basic code',
+      summary: 'akaa sdjadjakcja',
+      dueDate: '2024-10-11',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Learning JS',
+      summary: 'adasdasw qoei01asdjs',
+      dueDate: '2024-08-23',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
